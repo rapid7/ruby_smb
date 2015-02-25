06eaf7aaa0f677d6bc846c1ea949eef43099ec24
James Lee <egypt@metasploit.com>
2015-02-25 16:06:04 -0600

    Fix prerelease
    Dammit I swear I committed this before pushing.  >_<

--- lib/smb2/version.rb
+++ lib/smb2/version.rb
@@ -9,8 +9,6 @@ module Smb2
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 2
 
-    PRERELEASE='dispatch-as-namespace'
-
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
     #
