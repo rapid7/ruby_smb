db93e18a5a706e84fb979e8b3a0c719b49073af6
James Lee <egypt@metasploit.com>
2015-04-22 11:40:56 -0500

    Add docs

--- lib/smb2/file.rb
+++ lib/smb2/file.rb
@@ -2,8 +2,21 @@
 require 'smb2/packet/query/standard_information'
 
 class Smb2::File
+
+  # The tree that {Tree#create created} this File
+  #
+  # @return [Smb2::Tree]
   attr_accessor :tree
+
+  # The server's response from when we {Tree#create created} this File
+  #
+  # @return [Smb2::Packet::CreateResponse]
   attr_accessor :create_response
+
+  # The last response we got from {#read}. Useful for figuring out what went
+  # wrong.
+  #
+  # @return [Smb2::Packet::ReadResponse]
   attr_accessor :last_response
 
   def initialize(tree:, create_response:)
@@ -11,6 +24,10 @@ class Smb2::File
     self.create_response = create_response
   end
 
+
+  # Close this File handle on the server
+  #
+  # @return [Smb2::Packet::CloseResponse]
   def close
     packet = Smb2::Packet::CloseRequest.new do |request|
       request.file_id = self.create_response.file_id
@@ -21,6 +38,14 @@ class Smb2::File
     Smb2::Packet::CloseResponse.new(response)
   end
 
+  # Send a {Smb2::Packet::ReadRequest ReadRequest} and return the data.
+  #
+  # @note Does not yet handle files being too large for a single response.
+  #
+  # @param offset [Fixnum] offset from the beginning of the file (*default*: 0)
+  # @param length [Fixnum] number of bytes to read, starting from `offset`
+  #   (*default*: the whole file)
+  # @return [String]
   def read(offset: 0, length: self.create_response.end_of_file)
     packet = Smb2::Packet::ReadRequest.new do |request|
       request.read_offset = offset
