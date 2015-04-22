516192a456ab253792aa9049fa9a6447833afd01
James Lee <egypt@metasploit.com>
2015-04-22 10:48:34 -0500

    Add doc on why :struct_size is 9

--- lib/smb2/packet/tree_connect_request.rb
+++ lib/smb2/packet/tree_connect_request.rb
@@ -4,6 +4,10 @@ class Smb2::Packet
   # [Section 2.2.9 SMB2 TREE_CONNECT Request](https://msdn.microsoft.com/en-us/library/cc246567.aspx)
   class TreeConnectRequest < Smb2::Packet
     nest :header, RequestHeader
+    # "The client MUST set this field to 9, indicating the size of the request
+    # structure, not including the header. The client MUST set it to this
+    # value regardless of how long Buffer[] actually is in the request being
+    # sent."
     unsigned :struct_size, 16, default: 9
     # These two bytes are used in the response, but just padding in the
     # request
