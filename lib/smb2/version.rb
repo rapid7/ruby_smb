459f235e4911f6841c7a2663a83a3d90626a41cf
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-28 12:35:46 -0500

    Remove {PRERELEASE} links that don't work on master

--- lib/smb2/version.rb
+++ lib/smb2/version.rb
@@ -9,10 +9,10 @@ module Smb2
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 3
 
-    # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
+    # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
     #
-    # @return [String] '{MAJOR}.{MINOR}.{PATCH}' on master.  '{MAJOR}.{MINOR}.{PATCH}-{PRERELEASE}' on any branch
+    # @return [String] '{MAJOR}.{MINOR}.{PATCH}' on master.  '{MAJOR}.{MINOR}.{PATCH}-PRERELEASE' on any branch
     #   other than master.
     def self.full
       version = "#{MAJOR}.#{MINOR}.#{PATCH}"
@@ -24,10 +24,10 @@ module Smb2
       version
     end
 
-    # The full gem version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
+    # The full gem version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://guides.rubygems.org/specification-reference/#version RubyGems versioning} format.
     #
-    # @return [String] '{MAJOR}.{MINOR}.{PATCH}' on master.  '{MAJOR}.{MINOR}.{PATCH}.{PRERELEASE}' on any branch
+    # @return [String] '{MAJOR}.{MINOR}.{PATCH}' on master.  '{MAJOR}.{MINOR}.{PATCH}.PRERELEASE' on any branch
     #   other than master.
     def self.gem
       full.gsub('-', '.pre.')
