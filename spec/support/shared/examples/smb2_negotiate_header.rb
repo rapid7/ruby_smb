7e03de30d63912ea51971b14c914f0008251cf99
David Maloney <DMaloney@rapid7.com>
2015-07-24 12:27:42 -0500

    moar single quotes!

--- spec/support/shared/examples/smb2_negotiate_header.rb
+++ spec/support/shared/examples/smb2_negotiate_header.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 RSpec.shared_examples 'smb2_negotiate_packet_header' do
   specify do
-    expect(packet.magic).to eq("\xfeSMB".force_encoding("binary"))
+    expect(packet.magic).to eq("\xfeSMB".force_encoding('binary'))
   end
   specify do
-    expect(packet.signature).to eq(("\x00" * 16).force_encoding("binary"))
+    expect(packet.signature).to eq(("\x00" * 16).force_encoding('binary'))
   end
   specify do
     expect(packet.command).to eq(RubySMB::Smb2::COMMANDS[:NEGOTIATE])
