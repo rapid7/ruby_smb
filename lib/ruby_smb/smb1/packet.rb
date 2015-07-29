7800be4e97134f4c7f78986d4d0a23aa6857c6da
David Maloney <DMaloney@rapid7.com>
2015-07-29 14:13:09 -0500

    add some missing YARD docs
    added YARD docs to the namespace modules
    
    MSP-12908

--- lib/ruby_smb/smb1/packet.rb
+++ lib/ruby_smb/smb1/packet.rb
@@ -1,6 +1,7 @@
 require 'bit-struct'
 module RubySMB
   module Smb1
+    # This module holds the namespace for all SMB1 packets and related structures.
     module Packet
       autoload :SmbParameterBlock, 'ruby_smb/smb1/packet/smb_parameter_block'
     end
