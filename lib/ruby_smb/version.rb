4368c1feab537635d874cb8ddb9a7e75b3eb9430
Greg Mikeska <greg_mikeska@rapid7.com>
2015-07-29 11:08:31 -0500

    Remove Pre-release tag

--- lib/ruby_smb/version.rb
+++ lib/ruby_smb/version.rb
@@ -8,8 +8,6 @@ module RubySMB
     MINOR = 0
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 7
-
-    PRERELEASE = 'windows-error'
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
     #
