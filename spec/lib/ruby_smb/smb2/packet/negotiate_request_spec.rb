c947f90fa26489759a9ba92e745cfc8364cb0983
Trevor Rosen <trevor@catapult-creative.com>
2015-07-31 13:45:12 -0500

    Remove bare 'specify' from negotiate_request_spec

--- spec/lib/ruby_smb/smb2/packet/negotiate_request_spec.rb
+++ spec/lib/ruby_smb/smb2/packet/negotiate_request_spec.rb
@@ -4,12 +4,15 @@ RSpec.describe RubySMB::SMB2::Packet::NegotiateRequest do
   end
 
   context 'with packet bytes' do
+    let(:client_guid) { "ec1cb173f176e411af9e000c293f25dc" }
+    let(:dialects) { "020210020003" }
     let(:data) do
       [
         "fe534d42400001000000000000001f0000000000000000000000000000000000" \
         "fffe000000000000000000000000000000000000000000000000000000000000" \
-        "24000300010000007f000000ec1cb173f176e411af9e000c293f25dc00000000" \
-        "00000000020210020003"
+        "24000300010000007f000000#{client_guid}00000000" \
+        "00000000#{dialects}"
+
       ].pack('H*')
     end
 
@@ -17,33 +20,36 @@ RSpec.describe RubySMB::SMB2::Packet::NegotiateRequest do
     it_behaves_like 'request', RubySMB::SMB2::COMMANDS[:NEGOTIATE]
     it_behaves_like 'smb2_negotiate_packet_header'
 
-    context 'body' do
-      specify do
+    context 'packet body element hardcoded values' do
+      it 'packet.struct_size should equal 36' do
         expect(packet.struct_size).to eq(36)
       end
-      specify do
+
+      it 'packet.dialect_count should equal 3' do
         expect(packet.dialect_count).to eq(3)
       end
-      specify do
+
+      it 'packet.security_mode should equal 1' do
         expect(packet.security_mode).to eq(1)
       end
-      specify do
+
+      it 'packet.reserved should equal 0' do
         expect(packet.reserved).to eq(0)
       end
-      specify do
+
+      it 'packet.capabilities should equal 127' do
         # TODO constantize
         expect(packet.capabilities).to eq(127)
       end
 
-      specify do
-        expect(packet.client_guid).to eq(["ec1cb173f176e411af9e000c293f25dc"].pack("H*"))
+      it 'packet.client_guid should equal the specified value' do
+        expect(packet.client_guid).to eq([client_guid].pack("H*"))
       end
 
-      specify do
-        expect(packet.dialects).to eq(["020210020003"].pack("H*"))
+      it 'packet.dialects should equal the specified value' do
+        expect(packet.dialects).to eq([dialects].pack("H*"))
       end
 
     end
-
   end
 end
