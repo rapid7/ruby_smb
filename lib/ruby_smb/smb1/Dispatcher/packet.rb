746a2be0743e5d250bd8e11d99d9c3d2ca31f876
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-08-20 14:14:44 -0500

    checkpoint

--- lib/ruby_smb/smb1/Dispatcher/packet.rb
+++ lib/ruby_smb/smb1/Dispatcher/packet.rb
@@ -2,7 +2,17 @@ module RubySMB
 module SMB1
 # This module holds the namespace for all SMB1 packets and related structures.
 class Packet
-  # stub
+  def compose
+    # implies transmit
+    # build packet container
+    # fill with SMB_Packet we want to send
+  end
+
+  def decompose
+    # implies receive
+    # build packet container
+    # fill with SMB_Packet we expect to receive
+  end
 end
 end
 end
