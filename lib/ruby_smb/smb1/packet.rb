2b5ac783842d9e7487a372a66ffc39373e1430f3
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-08-20 00:22:56 -0500

    white

--- lib/ruby_smb/smb1/packet.rb
+++ lib/ruby_smb/smb1/packet.rb
@@ -1,8 +1,8 @@
 module RubySMB
 module SMB1
-  # This module holds the namespace for all SMB1 packets and related structures.
-  module Packet
-    # stub
-  end
+# This module holds the namespace for all SMB1 packets and related structures.
+module Packet
+  # stub
+end
 end
 end
