0576ead8746411c98477415350f83c59c5417b33
David Maloney <DMaloney@rapid7.com>
2015-08-25 17:54:22 -0500

    added labels to dialect class
    missed the dialect class when adding labels

--- lib/ruby_smb/smb1/packet/dialect.rb
+++ lib/ruby_smb/smb1/packet/dialect.rb
@@ -4,8 +4,8 @@ module RubySMB
       # This class represents the Dialect for a NegotiateRequest.
       # [2.2.4.52.1](https://msdn.microsoft.com/en-us/library/ee441572.aspx)
       class Dialect < BinData::Record
-        bit8 :buffer_format, :value => 0x2
-        stringz :dialect_string
+        bit8 :buffer_format,     :label => 'Buffer Format ID', :value => 0x2
+        stringz :dialect_string, :label => 'Dialect Name'
       end
     end
   end
