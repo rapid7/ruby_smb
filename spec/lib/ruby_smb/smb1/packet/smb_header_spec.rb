81ac157d4ac7c34eca3ca9f074122e8bd8077676
David Maloney <DMaloney@rapid7.com>
2015-08-27 12:25:16 -0500

    add specs for SMBHeader
    spec conversions continue on!

--- spec/lib/ruby_smb/smb1/packet/smb_header_spec.rb
+++ spec/lib/ruby_smb/smb1/packet/smb_header_spec.rb
@@ -15,10 +15,13 @@ RSpec.describe RubySMB::SMB1::Packet::SMBHeader do
   it { is_expected.to respond_to :uid }
   it { is_expected.to respond_to :mid }
 
+  it 'is little endian' do
+    expect(described_class.fields.instance_variable_get(:@endian)).to eq :little
+  end
+
   describe 'protocol' do
     it 'should be a 32-bit field per the SMB spec' do
-      protocol_size_field = header.fields.detect { |f| f.name == :protocol }
-      expect(protocol_size_field.length).to eq 32
+      expect(header.protocol).to be_a BinData::Bit32
     end
 
     it 'should be hardcoded to SMB_PROTOCOL_ID by default per the SMB spec' do
@@ -28,78 +31,67 @@ RSpec.describe RubySMB::SMB1::Packet::SMBHeader do
 
   describe 'command' do
     it 'should be a 8-bit field per the SMB spec' do
-      command_size_field = header.fields.detect { |f| f.name == :command }
-      expect(command_size_field.length).to eq 8
+      expect(header.command).to be_a BinData::Bit8
     end
   end
 
   describe 'nt_status' do
     it 'should be a 32-bit field per the SMB spec' do
-      nt_status_size_field = header.fields.detect { |f| f.name == :nt_status }
-      expect(nt_status_size_field.length).to eq 32
+      expect(header.nt_status).to be_a BinData::Bit32
     end
   end
 
   describe 'flags' do
     it 'should be a 8-bit field per the SMB spec' do
-      flags_size_field = header.fields.detect { |f| f.name == :flags }
-      expect(flags_size_field.length).to eq 8
+      expect(header.flags).to be_a BinData::Bit8
     end
   end
 
   describe 'flags2' do
     it 'should be a 16-bit field per the SMB spec' do
-      flags2_size_field = header.fields.detect { |f| f.name == :flags2 }
-      expect(flags2_size_field.length).to eq 16
+      expect(header.flags2).to be_a BinData::Bit16
     end
   end
 
   describe 'pid_high' do
     it 'should be a 16-bit field per the SMB spec' do
-      pid_high_size_field = header.fields.detect { |f| f.name == :pid_high }
-      expect(pid_high_size_field.length).to eq 16
+      expect(header.pid_high).to be_a BinData::Bit16
     end
   end
 
   describe 'security_features' do
     it 'should be a 64-bit field per the SMB spec' do
-      security_features_size_field = header.fields.detect { |f| f.name == :security_features }
-      expect(security_features_size_field.length).to eq 64
+      expect(header.security_features).to be_a BinData::Bit64
     end
   end
 
   describe 'reserved' do
     it 'should be a 16-bit field per the SMB spec' do
-      reserved_size_field = header.fields.detect { |f| f.name == :reserved }
-      expect(reserved_size_field.length).to eq 16
+      expect(header.reserved).to be_a BinData::Bit16
     end
   end
 
   describe 'tid' do
     it 'should be a 16-bit field per the SMB spec' do
-      tid_size_field = header.fields.detect { |f| f.name == :tid }
-      expect(tid_size_field.length).to eq 16
+      expect(header.tid).to be_a BinData::Bit16
     end
   end
 
   describe 'pid_low' do
     it 'should be a 16-bit field per the SMB spec' do
-      pid_low_size_field = header.fields.detect { |f| f.name == :pid_low }
-      expect(pid_low_size_field.length).to eq 16
+      expect(header.pid_low).to be_a BinData::Bit16
     end
   end
 
   describe 'uid' do
     it 'should be a 16-bit field per the SMB spec' do
-      uid_size_field = header.fields.detect { |f| f.name == :uid }
-      expect(uid_size_field.length).to eq 16
+      expect(header.uid).to be_a BinData::Bit16
     end
   end
 
   describe 'mid' do
     it 'should be a 16-bit field per the SMB spec' do
-      mid_size_field = header.fields.detect { |f| f.name == :mid }
-      expect(mid_size_field.length).to eq 16
+      expect(header.mid).to be_a BinData::Bit16
     end
   end
 end
