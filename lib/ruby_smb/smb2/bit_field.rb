00fea857b80debd9395e1c650cca8b2af5f9dd8f
David Maloney <DMaloney@rapid7.com>
2015-09-17 11:33:21 -0500

    add SMB2 capabailities bitfield
    added the smb2 capabailities bitfield class

--- lib/ruby_smb/smb2/bit_field.rb
+++ lib/ruby_smb/smb2/bit_field.rb
@@ -4,6 +4,7 @@ module RubySMB
 
       require 'ruby_smb/smb2/bit_field/smb2_header_flags'
       require 'ruby_smb/smb2/bit_field/smb2_security_mode'
+      require 'ruby_smb/smb2/bit_field/smb2_capabailities'
     end
   end
 end
