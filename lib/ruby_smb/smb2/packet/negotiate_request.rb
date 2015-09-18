3859ae6e9d07ab4429f03a5109ee186b5005f6ed
David Maloney <DMaloney@rapid7.com>
2015-09-18 10:39:14 -0500

    full negotoate request
    finished SMB2 negotiate request with specs

--- lib/ruby_smb/smb2/packet/negotiate_request.rb
+++ lib/ruby_smb/smb2/packet/negotiate_request.rb
@@ -4,8 +4,21 @@ module RubySMB
       # An SMB2 NEGOTIATE Request packet as defined by
       # [2.2.3 SMB2 NEGOTIATE Request](https://msdn.microsoft.com/en-us/library/cc246543.aspx)
       class NegotiateRequest < RubySMB::GenericPacket
-        endian      :little
-        smb2_header :smb2_header
+        endian              :little
+        smb2_header         :smb2_header
+        uint16              :structure_size,      :label => 'Structure Size',     :initial_value => 36
+        uint16              :dialect_count,       :label => 'Dialect Count'
+        smb2_security_mode  :security_mode
+        uint16              :reserved1,           :label => 'Reserved',           :initial_value => 0
+        smb2_capabilities   :capabilities
+        string              :client_guid,         :label => 'Client GUID',        :length => 16
+        file_time           :client_start_time,   :label => 'Client Start Time',  :initial_value => 0
+        array               :dialects,            :label => 'Dialects',           :type => :uint16,       :read_until => :eof
+
+        def initialize_instance
+          super
+          self.smb2_header.command = RubySMB::SMB2::Commands::NEGOTIATE
+        end
       end
     end
   end
