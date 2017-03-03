e7b3a64ecbe2f04a4dce51cede9603ffe32428ad
David Maloney <DMaloney@rapid7.com>
2017-03-03 10:54:21 -0600

    100% coverage for stringz16

--- spec/lib/ruby_smb/field/stringz16_spec.rb
+++ spec/lib/ruby_smb/field/stringz16_spec.rb
@@ -25,7 +25,26 @@ RSpec.describe RubySMB::Field::Stringz16 do
     it 'drops everything after the null terminator' do
       expect(null_terminator).to eq "ABCD".encode("utf-16le")
     end
+  end
 
+  context 'when reading data in' do
+
+    it 'stops at the first double null byte' do
+      io = StringIO.new("A\x00B\x00C\x00D\x00\x00\x00E\x00F\x00")
+      stringz16.read(io)
+      expect(stringz16.to_binary_s).to eq "A\x00B\x00C\x00D\x00\x00\x00"
+    end
+
+    it 'handles a zero length string' do
+      io = StringIO.new("\x00\x00A\x00")
+      stringz16.read(io)
+      expect(stringz16).to eq ""
+    end
+
+    it 'fails if no null terminator is found' do
+      io = StringIO.new("A\x00B\x00C\x00D\x00")
+      expect{ stringz16.read(io) }.to raise_error(EOFError)
+    end
   end
 
 end
