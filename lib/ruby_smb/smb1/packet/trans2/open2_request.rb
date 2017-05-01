afecc6421e9692e492af09653b9cc322296a4dcc
David Maloney <DMaloney@rapid7.com>
2017-05-01 12:46:38 -0500

    start getting Open2 subcommand working
    trying to get trans2 subcommands working
    starting with open2
    
    MS-2658

---
+++ lib/ruby_smb/smb1/packet/trans2/open2_request.rb
@@ -0,0 +1,24 @@
+module RubySMB
+  module SMB1
+    module Packet
+      module Trans2
+
+        # A Trans2 OPEN2 Request Packet as defined in
+        # [2.2.6.1.1 Request](https://msdn.microsoft.com/en-us/library/ee441733.aspx)
+        class Open2Request < BinData::Record
+
+          class ParameterBlock < RubySMB::SMB1::Packet::Trans2::Request::ParameterBlock
+          end
+
+          class DataBlock < RubySMB::SMB1::DataBlock
+
+          end
+
+          smb_header        :smb_header
+          parameter_block   :parameter_block
+          data_block        :data_block
+        end
+      end
+    end
+  end
+end
---
+++ lib/ruby_smb/smb1/packet/trans2/subcommands.rb
@@ -0,0 +1,11 @@
+module RubySMB
+  module SMB1
+    module Packet
+      module Trans2
+        module Subcommands
+          OPEN2 = 0x0000
+        end
+      end
+    end
+  end
+end
