33d5e35beb73df69ce1177662f580070882e2ef3
David Maloney <DMaloney@rapid7.com>
2017-05-04 14:28:22 -0500

    open2 trans2params block
    finished work on the trans2 paramaters structure
    for an open2 request
    
    MS-2658

--- lib/ruby_smb/field.rb
+++ lib/ruby_smb/field.rb
@@ -1,6 +1,7 @@
 module RubySMB
   module Field
     require 'ruby_smb/field/file_time'
+    require 'ruby_smb/field/utime'
     require 'ruby_smb/field/stringz16'
     require 'ruby_smb/field/nt_status'
   end
--- lib/ruby_smb/smb1/packet/trans2/open2_request.rb
+++ lib/ruby_smb/smb1/packet/trans2/open2_request.rb
@@ -11,8 +11,18 @@ module RubySMB
           end
 
           class Trans2Parameters < BinData::Record
-            open2_flags       :flags,       label: 'Flags'
-            open2_access_mode :access_mode, label: 'AccessMode'
+            endian :little
+            open2_flags         :flags,           label: 'Flags'
+            open2_access_mode   :access_mode,     label: 'AccessMode'
+            uint16              :reserved,        label: 'Reserved Space'
+            smb_file_attributes :file_attributes, label: 'File Attributes'
+            utime               :creation_time,   label: 'Creation Time'
+            open2_open_mode     :open_mode,       label: 'Open Mode'
+            uint32              :allocation_size, label: 'Allocation Size'
+            array               :reserved,        initial_length: 5 do
+              uint16 value: 0x0000
+            end
+            stringz             :filename,        label: 'Filename'
           end
 
           class DataBlock < RubySMB::SMB1::DataBlock
