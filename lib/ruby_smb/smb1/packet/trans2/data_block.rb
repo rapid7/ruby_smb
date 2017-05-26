78633019d93d92b6c2036befe0068ff40684ea9c
David Maloney <DMaloney@rapid7.com>
2017-05-26 11:16:36 -0500

    allow transact padding to be disabled
    added an option to disable the automatic
    padding on Trans2 DataBlocks

--- lib/ruby_smb/smb1/packet/trans2/data_block.rb
+++ lib/ruby_smb/smb1/packet/trans2/data_block.rb
@@ -6,24 +6,42 @@ module RubySMB
         # Extends the {RubySMB::SMB1::DataBlock} to include padding methods
         # that all Trans2 DataBlocks will need to handle proper byte alignment.
         class DataBlock < RubySMB::SMB1::DataBlock
+          # Controls whether the padding fields will be used
+          # @!attribute [rw] enable_padding
+          #   @return [Boolean]
+          attr_accessor :enable_padding
+
+          def initialize_instance
+            super
+            @enable_padding = true
+          end
+
           private
 
           # Determines the correct length for the padding in front of
           # #trans2_parameters. It should always force a 4-byte alignment.
           def pad1_length
-            if self.respond_to?(:name)
-              offset = (name.abs_offset + 1) % 4
+            if enable_padding
+              if self.respond_to?(:name)
+                offset = (name.abs_offset + 1) % 4
+              else
+                offset = (byte_count.abs_offset + 2) % 4
+              end
+              (4 - offset) % 4
             else
-              offset = (byte_count.abs_offset + 2) % 4
+              0
             end
-            (4 - offset) % 4
           end
 
           # Determines the correct length for the padding in front of
           # #trans2_data. It should always force a 4-byte alignment.
           def pad2_length
-            offset = (trans2_parameters.abs_offset + trans2_parameters.length) % 4
-            (4 - offset) % 4
+            if enable_padding
+              offset = (trans2_parameters.abs_offset + trans2_parameters.length) % 4
+              (4 - offset) % 4
+            else
+              0
+            end
           end
         end
       end
