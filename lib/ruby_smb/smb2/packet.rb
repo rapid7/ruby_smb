40b6239157f8cbf21af0db1b1a9e8f0fbf320c3a
David Maloney <DMaloney@rapid7.com>
2017-08-30 15:19:49 -0500

    SMB2 Close Response packet
    MS-2807

--- lib/ruby_smb/smb2/packet.rb
+++ lib/ruby_smb/smb2/packet.rb
@@ -21,6 +21,7 @@ module RubySMB
       require 'ruby_smb/smb2/packet/read_request'
       require 'ruby_smb/smb2/packet/read_response'
       require 'ruby_smb/smb2/packet/close_request'
+      require 'ruby_smb/smb2/packet/close_response'
     end
   end
 end
