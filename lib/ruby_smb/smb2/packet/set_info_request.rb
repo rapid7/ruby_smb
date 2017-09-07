4bfb61e0a113e80bedac1d3338aa9b2747fdb7cd
dmohanty-r7 <Dev_Mohanty@rapid7.com>
2017-09-07 12:24:35 -0500

    Add SET_INFO request/response

---
+++ lib/ruby_smb/smb2/packet/set_info_request.rb
@@ -0,0 +1,27 @@
+module RubySMB
+  module SMB2
+    module Packet
+      # An SMB2 Set Info Request Packet as defined in
+      # [2.2.39 SMB2 SET_INFO Request](https://msdn.microsoft.com/en-us/library/cc246560.aspx)
+      class SetInfoRequest < RubySMB::GenericPacket
+        endian :little
+
+        smb2_header           :smb2_header
+        uint16                :structure_size,         label: 'Structure Size',        initial_value: 33
+        uint8                 :info_type,              label: 'Info Type',             initial_value: 0x01
+        uint8                 :file_info_class,        label: 'File Info Class'
+        uint32                :buffer_length,          label: 'Buffer Length'
+        uint16                :buffer_offset,          label: 'Buffer Offset'
+        uint16                :reserved,               label: 'Reserved',              initial_value: 1
+        uint32                :additional_information, label: 'Additional Information' initial_value: 0
+        smb2_fileid           :file_id,                label: 'File ID'
+        string                :buffer,                 label: 'Buffer',                initial_value: 0x00
+
+        def initialize_instance
+          super
+          smb2_header.command = RubySMB::SMB2::Commands::SET_INFO
+        end
+      end
+    end
+  end
+end
