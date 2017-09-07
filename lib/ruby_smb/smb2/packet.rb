7d0f278571752ba5e57d44057d33394fe895327d
David Maloney <DMaloney@rapid7.com>
2017-09-07 11:39:35 -0500

    add SMB2 Write Request packet
    add the Request packet for an SMB2 Write command
    
    MS-2431

--- lib/ruby_smb/smb2/packet.rb
+++ lib/ruby_smb/smb2/packet.rb
@@ -24,6 +24,7 @@ module RubySMB
       require 'ruby_smb/smb2/packet/read_response'
       require 'ruby_smb/smb2/packet/close_request'
       require 'ruby_smb/smb2/packet/close_response'
+      require 'ruby_smb/smb2/packet/write_request'
     end
   end
 end
