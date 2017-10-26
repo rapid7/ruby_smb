abb50460b1dd66cf0912565471e50855e5858a1a
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2017-10-26 14:47:30 -0300

    Update specs

--- spec/lib/ruby_smb/fscc/file_information/file_disposition_information_spec.rb
+++ spec/lib/ruby_smb/fscc/file_information/file_disposition_information_spec.rb
@@ -3,11 +3,19 @@ require 'spec_helper'
 RSpec.describe RubySMB::Fscc::FileInformation::FileDispositionInformation do
   subject(:struct) { described_class.new }
 
-  it { should respond_to :buffer_length }
-  it { should respond_to :buffer_offset }
-  it { should respond_to :buffer }
+  it { should respond_to :delete_pending }
 
   it 'is little endian' do
     expect(described_class.fields.instance_variable_get(:@hints)[:endian]).to eq :little
+  end
+
+  describe '#delete_pending' do
+    it 'is a 8-bit field' do
+      expect(struct.delete_pending).to be_a BinData::Uint8
+    end
+
+    it 'should have a default value of 0' do
+      expect(struct.delete_pending).to eq 0
+    end
   end
 end
