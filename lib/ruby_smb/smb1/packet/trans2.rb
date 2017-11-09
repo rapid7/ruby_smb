b15944495ca8f1a76fffb41cd80928fbd6b9085b
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2017-11-08 21:23:37 -0300

    Add SetFileInformation request and response

--- lib/ruby_smb/smb1/packet/trans2.rb
+++ lib/ruby_smb/smb1/packet/trans2.rb
@@ -16,6 +16,8 @@ module RubySMB
         require 'ruby_smb/smb1/packet/trans2/find_first2_response'
         require 'ruby_smb/smb1/packet/trans2/find_next2_request'
         require 'ruby_smb/smb1/packet/trans2/find_next2_response'
+        require 'ruby_smb/smb1/packet/trans2/set_file_information_request'
+        require 'ruby_smb/smb1/packet/trans2/set_file_information_response'
       end
     end
   end
