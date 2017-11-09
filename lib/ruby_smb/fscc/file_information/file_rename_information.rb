e8b6cdbd3c2ac4253938f0bce31356bc1333ba46
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2017-11-08 21:27:08 -0300

    Update FileRenameInformation to be used by both SMB1 and SMB2

--- lib/ruby_smb/fscc/file_information/file_rename_information.rb
+++ lib/ruby_smb/fscc/file_information/file_rename_information.rb
@@ -8,13 +8,35 @@ module RubySMB
 
         endian :little
 
-        uint8  :replace_if_exists,  label: 'Replace If Exists'
-        uint8  :reserved_0,         label: 'Reserved 0',        initial_value: 0
-        uint16 :reserved_1,         label: 'Reserved 1',        initial_value: 0
-        uint32 :reserved_2,         label: 'Reserved 2',        initial_value: 0
-        uint64 :root_firectory,     label: 'Root Directory',    initial_value: 0
-        uint32 :file_name_length,   label: 'File Name Length',  initial_value: -> { file_name.do_num_bytes }
-        string :file_name,          label: 'File Name',           read_length: -> { file_name_length }
+        uint8  :replace_if_exists, label: 'Replace If Exists'
+
+        choice :reserved, selection: -> { get_smb_version } do
+          uint24 1, label: 'Reserved Space'
+          uint56 2, label: 'Reserved Space'
+        end
+
+        choice :root_directory, selection: -> { get_smb_version } do
+          uint32 1, label: 'Root Directory', initial_value: 0
+          uint64 2, label: 'Root Directory', initial_value: 0
+        end
+
+        uint32 :file_name_length, label: 'File Name Length', initial_value: -> { file_name.do_num_bytes }
+        string :file_name,        label: 'File Name',        read_length: -> { file_name_length }
+
+        def get_smb_version(obj = self)
+          # Return version 1 by default in case the structure is not part of a
+          # SMB packet. This way, we can still instantiate this structure
+          # independently without breaking the "choice" logic.
+          return 1 if obj.nil?
+          smb_version = if obj.respond_to?(:smb_header)
+                          1
+                        elsif obj.respond_to?(:smb2_header)
+                          2
+                        else
+                          get_smb_version(obj.parent)
+                        end
+          return smb_version
+        end
 
       end
     end
