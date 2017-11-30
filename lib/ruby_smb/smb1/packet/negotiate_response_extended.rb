d20d2a0f36db391ba1350704882e0ab6209d8dbf
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2017-11-30 15:21:27 +0100

    Update the Client negotiation methods to store the negotiated dialect
    Also, add helper methods to the SMB1 NegotiateResponse and
    NegotiateResponseExtended classes.

--- lib/ruby_smb/smb1/packet/negotiate_response_extended.rb
+++ lib/ruby_smb/smb1/packet/negotiate_response_extended.rb
@@ -41,6 +41,28 @@ module RubySMB
           return false unless parameter_block.capabilities.extended_security == 1
           true
         end
+
+        # Stores the list of {RubySMB::SMB1::Dialect} that were sent to the
+        # peer/server in the related Negotiate Request. This will be used by
+        # the {#negotiated_dialect} method.
+        #
+        # @param dialects [Array] array of {RubySMB::SMB1::Dialect}
+        # @return dialects [Array] array of {RubySMB::SMB1::Dialect}
+        # @raise [ArgumentError] if dialects is not an array of {RubySMB::SMB1::Dialect}
+        def dialects=(dialects)
+          unless dialects.all? { |dialect| dialect.is_a? Dialect }
+            raise ArgumentError, 'Dialects must be an array of Dialect objects'
+          end
+          @dialects = dialects
+        end
+
+        # Returns the negotiated dialect identifier
+        #
+        # @return [String] the negotiated dialect identifier or an empty string if the list of {RubySMB::SMB1::Dialect} was not provided.
+        def negotiated_dialect
+          return '' if @dialects.nil? || @dialects.empty?
+          @dialects[parameter_block.dialect_index].dialect_string
+        end
       end
     end
   end
