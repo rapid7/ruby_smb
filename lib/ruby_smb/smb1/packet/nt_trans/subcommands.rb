3b49ac02628701752299166021c53f2d75fefc8b
zerosum0x0 <zerosum0x0@users.noreply.github.com>
2018-01-26 15:26:09 -0700

    Add more NT Trans Function enums

--- lib/ruby_smb/smb1/packet/nt_trans/subcommands.rb
+++ lib/ruby_smb/smb1/packet/nt_trans/subcommands.rb
@@ -3,7 +3,14 @@ module RubySMB
     module Packet
       module NtTrans
         module Subcommands
-          CREATE = 0x0001
+          CREATE              = 0x0001
+          IOCTL               = 0x0002
+          SET_SECURITY_DESC   = 0x0003
+          NOTIFY              = 0x0004
+          RENAME              = 0x0005
+          QUERY_SECURITY_DESC = 0x0006
+          GET_USER_QUOTA      = 0x0007
+          SET_USER_QUOTE      = 0x0008
         end
       end
     end
