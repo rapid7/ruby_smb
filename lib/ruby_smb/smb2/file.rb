b842eb56d473e85363349887db87c0d53d3ab393
dmohanty-r7 <Dev_Mohanty@rapid7.com>
2018-02-07 09:36:23 -0600

    Add utils module to client

--- lib/ruby_smb/smb2/file.rb
+++ lib/ruby_smb/smb2/file.rb
@@ -133,6 +133,13 @@ module RubySMB
         read_request.offset       = offset
         read_request
       end
+      
+      def send_recv_read(read_length: 0, offset: 0)
+        read_request = read_packet(read_length: read_length, offset: offset)
+        raw_response = tree.client.send_recv(read_request)
+        response = RubySMB::SMB2::Packet::ReadResponse.read(raw_response)
+        response.buffer.to_binary_s
+      end
 
       # Delete a file on close
       #
@@ -200,6 +207,13 @@ module RubySMB
         write_request.write_offset  = offset
         write_request.buffer        = data
         write_request
+      end
+      
+      def send_recv_write(data:'', offset: 0)
+        pkt = write_packet(data: data, offset: offset)
+        raw_response = tree.client.send_recv(pkt)
+        response = RubySMB::SMB2::Packet::WriteResponse.read(raw_response)
+        response.write_count
       end
       
       # Rename a file
