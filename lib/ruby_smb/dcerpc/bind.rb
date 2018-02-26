1280e3b85e965b47c756d52c6e5f1994f824d3ea
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2018-02-26 13:56:18 -0300

    Update Bind and add p_syntax_id_t class

--- lib/ruby_smb/dcerpc/bind.rb
+++ lib/ruby_smb/dcerpc/bind.rb
@@ -1,17 +1,17 @@
 module RubySMB
   module Dcerpc
-    # The FileDirectoryInformation Class as defined in
-    # http://pubs.opengroup.org/onlinepubs/9629399/chap12.htm
-
+    # The Bind PDU as defined in
+    # [The bind PDU](http://pubs.opengroup.org/onlinepubs/9629399/chap12.htm#tagcjh_17_06_04_03)
     class PContElemT < BinData::Record
       endian :little
 
       uint16 :p_cont_id
       uint8 :n_transfer_syn, initial_value: 1
       uint8 :reserved
-      choice :abstract_syntax, selection: -> {endpoint} do
-        srv_svc_syntax Srvsvc
-      end
+      p_syntax_id_t :abstract_syntax,
+        uuid: ->      { endpoint::UUID },
+        ver_major: -> { endpoint::VER_MAJOR },
+        ver_minor: -> { endpoint::VER_MINOR }
       array :transfer_syntaxes, type: :ndr_syntax, initial_length: -> { n_transfer_syn }
     end
 
@@ -28,33 +28,20 @@ module RubySMB
     class Bind < BinData::Record
       endian :little
 
-      uint8 :rpc_vers, label: 'RPC version', initial_value: 5
-      uint8 :rpc_vers_minor, label: 'minor version'
-      uint8 :ptype, label: 'bind PDU', initial_value: 11
-      #uint8 :pfc_flags, label: 'flags', initial_value: 0x03
-      struct :pfc_flags do
-        bit1  :object
-        bit1  :maybe
-        bit1  :did_not_execute
-        bit1  :multiplex
-        bit1  :reserved
-        bit1  :cancel
-        bit1  :last_frag,  initial_value: 1
-        bit1  :first_frag, initial_value: 1
+      pdu_header :pdu_header,        label: 'PDU header'
+
+      uint16 :max_xmit_frag,         label: 'max transmit frag size',    initial_value: 0xFFFF
+      uint16 :max_recv_frag,         label: 'max receive  frag size',    initial_value: 0xFFFF
+      uint32 :assoc_group_id,        label: 'ncarnation of client-server assoc group'
+
+      p_cont_list_t :p_context_list, label: 'Presentation context list', endpoint: -> { endpoint }
+      string :auth_verifier,         label: 'Authentication verifier',   onlyif: -> { pdu_header.auth_length > 0 }
+
+      def initialize_instance
+        super
+        pdu_header.ptype = RubySMB::Dcerpc::PTypes::BIND
       end
-
-      uint32 :packed_drep, label: 'NDR data rep format label', initial_value: 16
-
-      uint16 :frag_length, label: 'total length of fragment', initial_value: 72
-      uint16 :auth_length, label: 'length of auth_value'
-      uint32 :call_id, label: 'call identifier', initial_value: 1
-
-      uint16 :max_xmit_frag, label: 'max transmit frag size', initial_value: 65535
-      uint16 :max_recv_frag, label: 'max receive  frag size', initial_value: 65535
-      uint32 :assoc_group_id, label: 'ncarnation of client-server assoc group'
-
-      p_cont_list_t :p_context_elem, endpoint: -> { endpoint }
-      string :auth_verifier_co_t
     end
   end
 end
+
