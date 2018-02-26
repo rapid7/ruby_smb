8f04401cb5ce24dfa886c71caa5973ee159b3a99
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2018-02-26 13:48:24 -0300

    Add DCERPC error classes

---
+++ lib/ruby_smb/dcerpc/error.rb
@@ -0,0 +1,12 @@
+module RubySMB
+  # Contains all the DCERPC specific Error classes.
+  module Dcerpc
+    module Error
+      # Base class for DCERPC errors
+      class DcerpcError < RubySMB::Error::RubySMBError; end
+
+      # Raised when The Bind operation fails
+      class BindError < DcerpcError; end
+    end
+  end
+end
