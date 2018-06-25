dd463fcda4ee7ffc199f382fb414e826b4c47ec2
Brent Cook <busterb@gmail.com>
2018-06-25 16:53:26 -0500

    add utility method for deleting files and what-not

--- lib/ruby_smb/client/utils.rb
+++ lib/ruby_smb/client/utils.rb
@@ -4,12 +4,12 @@ module RubySMB
 
       attr_accessor :tree_connects
       attr_accessor :open_files
-      
+
       attr_accessor :use_ntlmv2, :usentlm2_session, :send_lm, :use_lanman_key, :send_ntlm, :spnopt
       attr_accessor :evasion_opts
-      
+
       attr_accessor :native_os, :native_lm, :verify_signature, :auth_user
-      
+
       attr_accessor :last_file_id
 
       def last_tree
@@ -25,7 +25,7 @@ module RubySMB
       end
 
       def open(path, disposition=RubySMB::Dispositions::FILE_OPEN, write: false, read: true)
-         file = last_tree.open_file(filename: path, write: write, read: read, disposition: disposition)
+         file = last_tree.open_file(filename: path.gsub(/\\/, ''), write: write, read: read, disposition: disposition)
          @last_file_id = if file.respond_to?(:guid)
            file.guid.to_binary_s
          elsif file.respond_to?(:fid)
@@ -35,10 +35,10 @@ module RubySMB
          @last_file_id
       end
 
-      def create_pipe(path, disposition=RubySMB::Dispositions::FILE_OPEN_IF, impersonation)
+      def create_pipe(path, disposition=RubySMB::Dispositions::FILE_OPEN_IF)
         open(path.gsub(/\\/, ''), disposition, write: true, read: true)
       end
-      
+
       #Writes data to an open file handle
       def write(file_id, offset = 0, data = '', do_recv = true)
         @open_files[file_id].send_recv_write(data: data, offset: offset)
@@ -49,6 +49,12 @@ module RubySMB
         data.bytes
       end
 
+      def delete(path)
+        file = last_tree.open_file(filename: path.gsub(/\\/, ''), delete: true)
+        file.delete
+        file.close
+      end
+
       def close(file_id, tree_id)
        @open_files[file_id].close
       end
@@ -56,23 +62,23 @@ module RubySMB
       def tree_disconnect(share)
        @tree_connects.detect{|tree| tree.id == share }.disconnect!
       end
-      
+
       def native_os
         @peer_native
       end
-    
+
       def native_lm
         @native_lm
       end
-    
+
       def verify_signature
         @signing_required
       end
-    
+
       def auth_user
         @username
       end
-      
+
     end
   end
 end
