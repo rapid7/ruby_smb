0e6222737b099c076fc393dfad53ecd3556cee9c
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2019-04-01 16:15:10 -0300

    Add code and specs for BaseRegOpenKey packets

---
+++ lib/ruby_smb/dcerpc/winreg/open_key_response.rb
@@ -0,0 +1,25 @@
+module RubySMB
+  module Dcerpc
+    module Winreg
+
+      class PrpcHkey < Ndr::NdrContextHandle; end
+
+      # This class represents a BaseRegOpenKey Response Packet as defined in
+      # [3.1.5.15 BaseRegOpenKey (Opnum 15)](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-rrp/8cb48f55-19e1-4ea2-8d76-dd0f6934f0d9)
+      class OpenKeyResponse < BinData::Record
+        attr_reader :opnum
+
+        endian    :little
+        prpc_hkey :phk_result
+        uint32    :error_status
+
+        def initialize_instance
+          super
+          @opnum = REG_OPEN_KEY
+        end
+      end
+
+    end
+  end
+end
+
