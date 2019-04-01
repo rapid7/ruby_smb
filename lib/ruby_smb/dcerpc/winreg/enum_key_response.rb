d87cd72a9253d016834737bda5d34b1b3d05224e
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2019-04-01 13:00:02 -0300

    Add code and specs for BaseRegEnumKey packets

---
+++ lib/ruby_smb/dcerpc/winreg/enum_key_response.rb
@@ -0,0 +1,42 @@
+module RubySMB
+  module Dcerpc
+    module Winreg
+
+      # This class represents a BaseRegEnumKey Response Packet as defined in
+      # [3.1.5.10 BaseRegEnumKey (Opnum 9)](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-rrp/668627e9-e0eb-4ab1-911f-0af589beeac3)
+      class EnumKeyResponse < BinData::Record
+        attr_reader :opnum
+
+        endian :little
+
+        rrp_unicode_string  :lp_name
+        string              :pad1,     length: -> { pad_length1 }
+        prrp_unicode_string :lp_class, initial_value: 0
+        string              :pad2,     length: -> { pad_length2 }
+        ndr_lp_file_time    :lpft_last_write_time
+        uint32              :error_status
+
+        def initialize_instance
+          super
+          @opnum = REG_ENUM_KEY
+        end
+
+        # Determines the correct length for the padding in front of
+        # #lp_class. It should always force a 4-byte alignment.
+        def pad_length1
+          offset = (lp_name.abs_offset + lp_name.to_binary_s.length) % 4
+          (4 - offset) % 4
+        end
+
+        # Determines the correct length for the padding in front of
+        # #lpft_last_write_time. It should always force a 4-byte alignment.
+        def pad_length2
+          offset = (lp_class.abs_offset + lp_class.to_binary_s.length) % 4
+          (4 - offset) % 4
+        end
+      end
+
+    end
+  end
+end
+
---
+++ spec/lib/ruby_smb/dcerpc/winreg/enum_key_response_spec.rb
@@ -0,0 +1,97 @@
+RSpec.describe RubySMB::Dcerpc::Winreg::EnumKeyResponse do
+  subject(:packet) { described_class.new }
+
+  it { is_expected.to respond_to :lp_name }
+  it { is_expected.to respond_to :pad1 }
+  it { is_expected.to respond_to :lp_class }
+  it { is_expected.to respond_to :pad2 }
+  it { is_expected.to respond_to :lpft_last_write_time }
+  it { is_expected.to respond_to :error_status }
+  it { is_expected.to respond_to :opnum }
+
+  it 'is little endian' do
+    expect(described_class.fields.instance_variable_get(:@hints)[:endian]).to eq :little
+  end
+
+
+  describe '#lp_name' do
+    it 'is a RrpUnicodeString structure' do
+      expect(packet.lp_name).to be_a RubySMB::Dcerpc::RrpUnicodeString
+    end
+  end
+
+  describe '#pad1' do
+    it 'is a string' do
+      expect(packet.pad1).to be_a BinData::String
+    end
+
+    it 'should keep #lp_class 4-byte aligned' do
+      packet.lp_name = "test"
+      expect(packet.lp_class.abs_offset % 4).to eq 0
+    end
+  end
+
+  describe '#lp_class' do
+    it 'is a PrrpUnicodeString structure' do
+      expect(packet.lp_class).to be_a RubySMB::Dcerpc::PrrpUnicodeString
+    end
+
+    it 'has a initial value of 0' do
+      expect(packet.lp_class).to eq(0)
+    end
+  end
+
+  describe '#pad2' do
+    it 'is a string' do
+      expect(packet.pad2).to be_a BinData::String
+    end
+
+    it 'should keep #lpft_last_write_time 4-byte aligned' do
+      packet.lp_class = "test"
+      expect(packet.lpft_last_write_time.abs_offset % 4).to eq 0
+    end
+  end
+
+  describe '#lpft_last_write_time' do
+    it 'is a NdrLpFileTime structure' do
+      expect(packet.lpft_last_write_time).to be_a RubySMB::Dcerpc::Ndr::NdrLpFileTime
+    end
+  end
+
+  describe '#error_status' do
+    it 'is a 32-bit unsigned integer' do
+      expect(packet.error_status).to be_a BinData::Uint32le
+    end
+  end
+
+  describe '#initialize_instance' do
+    it 'sets #opnum to REG_ENUM_KEY constant' do
+      expect(packet.opnum).to eq(RubySMB::Dcerpc::Winreg::REG_ENUM_KEY)
+    end
+  end
+
+  describe '#pad_length1' do
+    it 'returns 0 when #lp_class is already 4-byte aligned' do
+      packet.lp_name = 'align'
+      expect(packet.pad_length1).to eq 0
+    end
+
+    it 'returns 2 when #lp_class is only 2-byte aligned' do
+      packet.lp_name = 'align' + 'A'
+      expect(packet.pad_length1).to eq 2
+    end
+  end
+
+  describe '#pad_length2' do
+    it 'returns 0 when #lpft_last_write_time is already 4-byte aligned' do
+      packet.lp_class = 'align'
+      expect(packet.pad_length2).to eq 0
+    end
+
+    it 'returns 2 when #lpft_last_write_time is only 2-byte aligned' do
+      packet.lp_class = 'align' + 'A'
+      expect(packet.pad_length2).to eq 2
+    end
+  end
+end
+
