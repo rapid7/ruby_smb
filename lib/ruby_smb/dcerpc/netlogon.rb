45a8d32159062c649f60de8b3e96f9cc059005a2
Spencer McIntyre <zeroSteiner@gmail.com>
2020-09-17 12:47:29 -0400

    Use a string instead of an int array for credential data

--- lib/ruby_smb/dcerpc/netlogon.rb
+++ lib/ruby_smb/dcerpc/netlogon.rb
@@ -13,8 +13,8 @@ module RubySMB
       NETR_SERVER_PASSWORD_SET2 = 30
 
       # see: https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-nrpc/d55e2632-7163-4f6c-b662-4b870e8cc1cd
-      class NetlogonCredential < BinData::Uint8Array
-        default_parameters initial_length: 8
+      class NetlogonCredential < BinData::String
+        default_parameters length: 8
       end
 
       # see: https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-nrpc/76c93227-942a-4687-ab9d-9d972ffabdab
