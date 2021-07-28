a952ce407ce8141efd0752da3d15c52c8ba3662a
Spencer McIntyre <zeroSteiner@gmail.com>
2021-07-28 18:02:19 -0400

    More work on session setup
    At this point Windows will send it's NTLM information but won't
    acknowledge the successful response.

--- lib/ruby_smb/server/server_client/session_setup.rb
+++ lib/ruby_smb/server/server_client/session_setup.rb
@@ -31,24 +31,29 @@ module RubySMB
           type1_msg = Net::NTLM::Message::Type1.parse(raw_type1_msg)
           type2_msg = Net::NTLM::Message::Type2.new.tap do |msg|
             msg.target_name = 'LOCALHOST'.encode('UTF-16LE').b
-            msg.flag = 0xe28a8235
+            msg.flag = 0xe28a8215
             msg.challenge = SecureRandom.bytes(8).unpack1('Q')
             target_info = Net::NTLM::TargetInfo.new('')
             target_info.av_pairs.merge!({
+              Net::NTLM::TargetInfo::MSV_AV_NB_DOMAIN_NAME => 'LOCALHOST'.encode('UTF-16LE').b,
               Net::NTLM::TargetInfo::MSV_AV_NB_COMPUTER_NAME => 'LOCALHOST'.encode('UTF-16LE').b,
               Net::NTLM::TargetInfo::MSV_AV_DNS_DOMAIN_NAME => "\x00\x00".b,
               Net::NTLM::TargetInfo::MSV_AV_DNS_COMPUTER_NAME => 'LOCALHOST'.encode('UTF-16LE').b,
-              Net::NTLM::TargetInfo::MSV_AV_CHANNEL_BINDINGS => "\x00\x00\x00\x00\x00\x00\x00\x00".b
+              Net::NTLM::TargetInfo::MSV_AV_TIMESTAMP => [(Time.now.to_i + Net::NTLM::TIME_OFFSET) * Field::FileTime::NS_MULTIPLIER].pack('Q')
             })
             msg.target_info = target_info.to_s
-            msg.enable(:context)
             msg.enable(:target_info)
+            msg.context = 0
+            msg.enable(:context)
+            msg.os_version = "\x06\x01\x00\x00\x00\x00\x00\x0f".b # Version 6.1 (Build 0); NTLM Current Revision 15
+            msg.enable(:os_version)
           end
 
           response = SMB2::Packet::SessionSetupResponse.new
           response.smb2_header.nt_status = WindowsError::NTStatus::STATUS_MORE_PROCESSING_REQUIRED.value
           response.smb2_header.credits = 1
           response.smb2_header.message_id = 1
+          response.smb2_header.session_id = SecureRandom.random_bytes(4).unpack1('V')
           response.set_type2_blob(type2_msg.serialize)
           send_packet(response)
           @state = :session_setup2
@@ -64,7 +69,8 @@ module RubySMB
             return
           end
 
-          raw_type3_msg = Gss.asn1dig(gss_api, 0, 0, 0)&.value
+          # fix this, sometimes it's 010 when the client is windows, otherwise it's 000 when the client is RubySMB
+          raw_type3_msg = Gss.asn1dig(gss_api, 0, 1, 0)&.value
           type3_msg = Net::NTLM::Message::Type1.parse(raw_type3_msg)
 
           gss = OpenSSL::ASN1::ASN1Data.new([
