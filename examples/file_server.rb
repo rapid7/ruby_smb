add9b0c7d0462045c560bac6ebea738bfde99aba
Spencer McIntyre <zeroSteiner@gmail.com>
2021-11-17 16:23:31 -0500

    Add command line options to the file server example

--- examples/file_server.rb
+++ examples/file_server.rb
@@ -1,17 +1,45 @@
 #!/usr/bin/ruby
 
 require 'bundler/setup'
+require 'optparse'
 require 'ruby_smb'
 require 'ruby_smb/gss/provider/ntlm'
 
-ntlm_provider = RubySMB::Gss::Provider::NTLM.new(allow_anonymous: true)
-ntlm_provider.put_account('RubySMB', 'password')  # password can also be an NTLM hash
+options = {
+  allow_anonymous: true,
+  domain: nil,
+  username: 'RubySMB',
+  password: 'password',
+  share_name: 'home'
+}
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename(__FILE__)} [options]"
+  opts.on("--share SHARE", "The share name (default: #{options[:share_name]})") do |share|
+    options[:share_name] = share
+  end
+  opts.on("--[no-]anonymous", "Allow anonymous access (default: #{options[:allow_anonymous]})") do |allow_anonymous|
+    options[:allow_anonymous] = allow_anonymous
+  end
+  opts.on("--username USERNAME", "The account's username (default: #{options[:username]})") do |username|
+    if username.include?('\\')
+      options[:domain], options[:username] = username.split('\\', 2)
+    else
+      options[:username] = username
+    end
+  end
+  opts.on("--password PASSWORD", "The account's password (default: #{options[:password]})") do |password|
+    options[:password] = password
+  end
+end.parse!
+
+ntlm_provider = RubySMB::Gss::Provider::NTLM.new(allow_anonymous: options[:allow_anonymous])
+ntlm_provider.put_account(options[:username], options[:password], domain: options[:domain])  # password can also be an NTLM hash
 
 server = RubySMB::Server.new(
   gss_provider: ntlm_provider,
   logger: :stdout
 )
-server.add_share(RubySMB::Server::Share::Provider::Disk.new('home', Dir.pwd))
+server.add_share(RubySMB::Server::Share::Provider::Disk.new(options[:share_name], Dir.pwd))
 puts "server is running"
 server.run do
   puts "received connection"
