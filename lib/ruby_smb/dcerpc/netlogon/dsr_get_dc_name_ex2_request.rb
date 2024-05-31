b56789305dea0a2e00079eba30418c099973506e
sud0Ru <sud0.ru.id@gmail.com>
2024-05-31 14:03:04 -0400

    adding the request file

---
+++ lib/ruby_smb/dcerpc/netlogon/dsr_get_dc_name_ex2_request.rb
@@ -0,0 +1,28 @@
+require 'ruby_smb/dcerpc/ndr'
+
+module RubySMB
+  module Dcerpc
+    module Netlogon
+
+      # [3.5.4.3.1 DsrGetDCNameEx2 (Opnum 34)](https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-nrpc/fb8e1146-a045-4c31-98d1-c68507ad5620)
+      class DsrGetDCNameEx2Request < BinData::Record
+        attr_reader :opnum
+
+        endian :little
+
+        logonsrv_handle  :computer_name
+        ndr_wide_stringz_ptr :account_name
+        ndr_uint32 :allowable_account_control_bits
+        ndr_wide_stringz_ptr :domain_name
+        uuid_ptr  :domain_guid
+        ndr_wide_stringz_ptr :site_name
+        ndr_uint32 :flags
+
+        def initialize_instance
+          super
+          @opnum = DSR_GET_DC_NAME_EX2
+        end
+      end
+    end
+  end
+end
