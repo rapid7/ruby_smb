f6b3c2de8d59928c521eba097e9dcf6499bc6834
Spencer McIntyre <zeroSteiner@gmail.com>
2026-04-28 16:21:39 -0400

    Update examples/read_registry_key_value.rb to use OptionParser
    Replace raw ARGV access with OptionParser following the existing convention.
    Adds --[no-]smbv1/2/3, --username (with domain\username splitting), and
    --password flags. Prints the help banner when a required positional is
    missing or when -h/--help is given.

--- examples/read_registry_key_value.rb
+++ examples/read_registry_key_value.rb
@@ -2,23 +2,69 @@
 
 # This example script is used for testing the Winreg registry key value read functionality.
 # It will attempt to connect to a host and reads the value of a specified registry key.
-# Example usage: ruby enum_registry_key.rb 192.168.172.138 msfadmin msfadmin HKLM\\My\\Key ValueName
+# Example usage: ruby read_registry_key_value.rb --username msfadmin --password msfadmin 192.168.172.138 HKLM\\My\\Key ValueName
 # This will try to connect to \\192.168.172.138 with the msfadmin:msfadmin credentialas and reads the ValueName data corresponding to the HKLM\\My\\Key registry key.
 
 require 'bundler/setup'
+require 'optparse'
 require 'ruby_smb'
 
-address      = ARGV[0]
-username     = ARGV[1]
-password     = ARGV[2]
-registry_key = ARGV[3]
-value_name   = ARGV[4]
-smb_versions = ARGV[5]&.split(',') || ['1','2','3']
+args = ARGV.dup
+options = {
+  domain: '.',
+  username: '',
+  password: '',
+  smbv1: true,
+  smbv2: true,
+  smbv3: true,
+  target: nil,
+  registry_key: nil,
+  value_name: nil
+}
+options[:value_name] = args.pop
+options[:registry_key] = args.pop
+options[:target] = args.pop
+optparser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename(__FILE__)} [options] target registry_key value_name"
+  opts.on("--[no-]smbv1", "Enable or disable SMBv1 (default: #{options[:smbv1] ? 'Enabled' : 'Disabled'})") do |smbv1|
+    options[:smbv1] = smbv1
+  end
+  opts.on("--[no-]smbv2", "Enable or disable SMBv2 (default: #{options[:smbv2] ? 'Enabled' : 'Disabled'})") do |smbv2|
+    options[:smbv2] = smbv2
+  end
+  opts.on("--[no-]smbv3", "Enable or disable SMBv3 (default: #{options[:smbv3] ? 'Enabled' : 'Disabled'})") do |smbv3|
+    options[:smbv3] = smbv3
+  end
+  opts.on("--username USERNAME", "The account's username (default: #{options[:username]})") do |username|
+    if username.include?('\\')
+      options[:domain], options[:username] = username.split('\\', 2)
+    else
+      options[:username] = username
+    end
+  end
+  opts.on("--password PASSWORD", "The account's password (default: #{options[:password]})") do |password|
+    options[:password] = password
+  end
+end
+optparser.parse!(args)
+
+if [options[:target], options[:registry_key], options[:value_name]].any? { |a| a == '-h' || a == '--help' }
+  puts optparser.help
+  exit
+end
+
+if options[:target].nil? || options[:registry_key].nil? || options[:value_name].nil?
+  abort(optparser.help)
+end
+
+address = options[:target]
+registry_key = options[:registry_key]
+value_name = options[:value_name]
 
 sock = TCPSocket.new address, 445
 dispatcher = RubySMB::Dispatcher::Socket.new(sock, read_timeout: 60)
 
-client = RubySMB::Client.new(dispatcher, smb1: smb_versions.include?('1'), smb2: smb_versions.include?('2'), smb3: smb_versions.include?('3'), username: username, password: password)
+client = RubySMB::Client.new(dispatcher, smb1: options[:smbv1], smb2: options[:smbv2], smb3: options[:smbv3], username: options[:username], password: options[:password], domain: options[:domain])
 protocol = client.negotiate
 status = client.authenticate
 
@@ -30,4 +76,3 @@ key_value = client.read_registry_key_value(address, registry_key, value_name)
 puts key_value
 
 client.disconnect!
-
