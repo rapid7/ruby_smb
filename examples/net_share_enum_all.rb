f3807f18499d51aaf14ac2d715cbf58fc78e2e3b
Spencer McIntyre <zeroSteiner@gmail.com>
2026-04-28 16:18:27 -0400

    Update examples/net_share_enum_all.rb to use OptionParser
    Replace raw ARGV access with OptionParser following the existing convention.
    Adds --[no-]smbv1/2/3, --username (with domain\username splitting), and
    --password flags. Prints the help banner when target is missing or when
    -h/--help is given.

--- examples/net_share_enum_all.rb
+++ examples/net_share_enum_all.rb
@@ -2,23 +2,63 @@
 
 # This example script is used for testing NetShareEnumAll functionality
 # It will attempt to connect to a host and enumerate shares.
-# Example usage: ruby net_share_enum_all.rb 192.168.172.138 msfadmin msfadmin
+# Example usage: ruby net_share_enum_all.rb --username msfadmin --password msfadmin 192.168.172.138
 # This will try to connect to \\192.168.172.138 with the msfadmin:msfadmin credentials
 
 require 'bundler/setup'
+require 'optparse'
 require 'ruby_smb'
 
-address      = ARGV[0]
-username     = ARGV[1]
-password     = ARGV[2]
-smb_versions = ARGV[3]&.split(',') || ['1','2','3']
+args = ARGV.dup
+options = {
+  domain: '.',
+  username: '',
+  password: '',
+  smbv1: true,
+  smbv2: true,
+  smbv3: true,
+  target: nil
+}
+options[:target] = args.pop
+optparser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename(__FILE__)} [options] target"
+  opts.on("--[no-]smbv1", "Enable or disable SMBv1 (default: #{options[:smbv1] ? 'Enabled' : 'Disabled'})") do |smbv1|
+    options[:smbv1] = smbv1
+  end
+  opts.on("--[no-]smbv2", "Enable or disable SMBv2 (default: #{options[:smbv2] ? 'Enabled' : 'Disabled'})") do |smbv2|
+    options[:smbv2] = smbv2
+  end
+  opts.on("--[no-]smbv3", "Enable or disable SMBv3 (default: #{options[:smbv3] ? 'Enabled' : 'Disabled'})") do |smbv3|
+    options[:smbv3] = smbv3
+  end
+  opts.on("--username USERNAME", "The account's username (default: #{options[:username]})") do |username|
+    if username.include?('\\')
+      options[:domain], options[:username] = username.split('\\', 2)
+    else
+      options[:username] = username
+    end
+  end
+  opts.on("--password PASSWORD", "The account's password (default: #{options[:password]})") do |password|
+    options[:password] = password
+  end
+end
+optparser.parse!(args)
 
-path     = "\\\\#{address}\\IPC$"
+if options[:target] == '-h' || options[:target] == '--help'
+  puts optparser.help
+  exit
+end
+
+if options[:target].nil?
+  abort(optparser.help)
+end
+
+address = options[:target]
 
 sock = TCPSocket.new address, 445
 dispatcher = RubySMB::Dispatcher::Socket.new(sock, read_timeout: 60)
 
-client = RubySMB::Client.new(dispatcher, smb1: smb_versions.include?('1'), smb2: smb_versions.include?('2'), smb3: smb_versions.include?('3'), username: username, password: password)
+client = RubySMB::Client.new(dispatcher, smb1: options[:smbv1], smb2: options[:smbv2], smb3: options[:smbv3], username: options[:username], password: options[:password], domain: options[:domain])
 protocol = client.negotiate
 status = client.authenticate
 
@@ -32,4 +72,3 @@ rescue => e
 end
 
 client.disconnect!
-
