9308c5bac241bd2f37a97aab06f1de9935797dc5
Spencer McIntyre <zeroSteiner@gmail.com>
2026-04-28 16:19:10 -0400

    Update examples/pipes.rb to use OptionParser
    Replace raw ARGV access with OptionParser following the existing convention.
    Adds --[no-]smbv1/2/3, --username (with domain\username splitting), and
    --password flags. Prints the help banner when a required positional is
    missing or when -h/--help is given.

--- examples/pipes.rb
+++ examples/pipes.rb
@@ -4,22 +4,66 @@
 # Example script for connecting to a named pipe and performing a peek operation.
 # This is used to demonstrate pipe operations.
 #
-# Usage: ruby pipes.rb ADDRESS PIPENAME USER PASS 1|2
+# Usage: ruby pipes.rb --username USER --password PASS ADDRESS PIPENAME
 #
 
 require 'bundler/setup'
+require 'optparse'
 require 'ruby_smb'
 
-address      = ARGV[0]
-pipename     = ARGV[1]
-username     = ARGV[2]
-password     = ARGV[3]
-smb_versions = ARGV[4]&.split(',') || ['1','2','3']
+args = ARGV.dup
+options = {
+  domain: '.',
+  username: '',
+  password: '',
+  smbv1: true,
+  smbv2: true,
+  smbv3: true,
+  target: nil,
+  pipename: nil
+}
+options[:pipename] = args.pop
+options[:target] = args.pop
+optparser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename(__FILE__)} [options] target pipename"
+  opts.on("--[no-]smbv1", "Enable or disable SMBv1 (default: #{options[:smbv1] ? 'Enabled' : 'Disabled'})") do |smbv1|
+    options[:smbv1] = smbv1
+  end
+  opts.on("--[no-]smbv2", "Enable or disable SMBv2 (default: #{options[:smbv2] ? 'Enabled' : 'Disabled'})") do |smbv2|
+    options[:smbv2] = smbv2
+  end
+  opts.on("--[no-]smbv3", "Enable or disable SMBv3 (default: #{options[:smbv3] ? 'Enabled' : 'Disabled'})") do |smbv3|
+    options[:smbv3] = smbv3
+  end
+  opts.on("--username USERNAME", "The account's username (default: #{options[:username]})") do |username|
+    if username.include?('\\')
+      options[:domain], options[:username] = username.split('\\', 2)
+    else
+      options[:username] = username
+    end
+  end
+  opts.on("--password PASSWORD", "The account's password (default: #{options[:password]})") do |password|
+    options[:password] = password
+  end
+end
+optparser.parse!(args)
+
+if [options[:target], options[:pipename]].any? { |a| a == '-h' || a == '--help' }
+  puts optparser.help
+  exit
+end
+
+if options[:target].nil? || options[:pipename].nil?
+  abort(optparser.help)
+end
+
+address = options[:target]
+pipename = options[:pipename]
 
 sock = TCPSocket.new(address, 445)
 dispatcher = RubySMB::Dispatcher::Socket.new(sock)
 
-client = RubySMB::Client.new(dispatcher, smb1: smb_versions.include?('1'), smb2: smb_versions.include?('2'), smb3: smb_versions.include?('3'), username: username, password: password)
+client = RubySMB::Client.new(dispatcher, smb1: options[:smbv1], smb2: options[:smbv2], smb3: options[:smbv3], username: options[:username], password: options[:password], domain: options[:domain])
 smbver = client.negotiate
 
 if smbver == 'SMB1'
